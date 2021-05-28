03b893b5f70fb2a2e663c6bace2e01fc2fc9e71a
Cheesemcpuff <joshuahall98@live.com>
2021-05-28 23:07:21 +0100

    Collide Walls

--- Eat the Enemy/Assets/Scripts/Player/PlayerController.cs
+++ Eat the Enemy/Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -25,7 +26,8 @@ public class PlayerController : MonoBehaviour
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                StartCoroutine(Move(targetPos));
+                if (IsWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
             }
         }
     }
@@ -42,5 +44,14 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+        return true;
     }
 }
